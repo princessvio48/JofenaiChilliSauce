be87d8193503ff4ee326295c3c2eb2334b197fd3
TFKprogrammer <rogerskarunde@gmail.com>
2022-10-19 19:24:25 +0300

    adding files to master

--- jofenaiChiliSauce/settings.py
+++ jofenaiChiliSauce/settings.py
@@ -76,16 +76,26 @@ WSGI_APPLICATION = 'jofenaiChiliSauce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pilipili',
+#         'PASSWORD': 'Catherine@27',
+#         'USER': 'postgres',
+#         'HOST': 'localhost',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pilipili',
-        'PASSWORD': 'Catherine@27',
-        'USER': 'postgres',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
