eb383277b0938b95eb8b016967a3393ca64632e1
TFKprogrammer <rogerskarunde@gmail.com>
2022-12-23 11:48:11 +0300

    adding name of site

--- jofenaiChiliSauce/urls.py
+++ jofenaiChiliSauce/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+admin.site.site_header = "JOFENAI ADMINISTRATION"
+admin.site.site_title = "jofenai"
+admin.site.index_title = "jofenai"
